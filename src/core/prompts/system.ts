58d2b4467522ad5a85b537a9ed7ae39182ea225c
shtse8 <shtse8@gmail.com>
2025-04-07 09:31:29 +0100

    refactor(system): Simplify placeholder substitutions in system prompts
    Replaced individual placeholder replacements with a mapping approach using a placeholder map. This enhances maintainability and readability of the code. The substitutions are now performed using a single reduce function, streamlining the process of generating the final prompt.

--- src/core/prompts/system.ts
+++ src/core/prompts/system.ts
@@ -170,95 +170,74 @@ export const SYSTEM_PROMPT = async (
 			{ language: language ?? formatLanguage(vscode.env.language), rooIgnoreInstructions },
 		)
 
-		// Perform substitutions
-		let finalPrompt = fileCustomSystemPrompt
-		finalPrompt = finalPrompt.replace(/\{\{ROLE_DEFINITION\}\}/g, roleDefinition)
-		finalPrompt = finalPrompt.replace(/\{\{SHARED_TOOL_USE_SECTION\}\}/g, sharedToolUseSection)
-		finalPrompt = finalPrompt.replace(/\{\{TOOL_DESCRIPTIONS\}\}/g, toolDescriptions)
-		finalPrompt = finalPrompt.replace(/\{\{TOOL_USE_GUIDELINES\}\}/g, toolUseGuidelines)
-		finalPrompt = finalPrompt.replace(/\{\{MCP_SERVERS_SECTION\}\}/g, mcpServersSection)
-		finalPrompt = finalPrompt.replace(/\{\{CAPABILITIES_SECTION\}\}/g, capabilitiesSection)
-		finalPrompt = finalPrompt.replace(/\{\{MODES_SECTION\}\}/g, modesSection)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_SECTION\}\}/g, rulesComponents.fullSection)
-		// Add granular rule placeholders
-		finalPrompt = finalPrompt.replace(/\{\{RULES_BASE_DIR\}\}/g, rulesComponents.baseDir)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_RELATIVE_PATHS\}\}/g, rulesComponents.relativePaths)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_NO_CD\}\}/g, rulesComponents.noCd)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_NO_HOME_CHAR\}\}/g, rulesComponents.noHomeChar)
-		finalPrompt = finalPrompt.replace(
-			/\{\{RULES_EXECUTE_COMMAND_CONTEXT\}\}/g,
-			rulesComponents.executeCommandContext,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_SEARCH_FILES_USAGE\}\}/g, rulesComponents.searchFilesUsage)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_NEW_PROJECT_STRUCTURE\}\}/g, rulesComponents.newProjectStructure)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_EDITING_TOOLS_AVAILABLE\}\}/g, rulesComponents.availableTools)
-		finalPrompt = finalPrompt.replace(
-			/\{\{RULES_EDITING_INSERT_CONTENT_DETAIL\}\}/g,
-			rulesComponents.insertContentDetail,
-		)
-		finalPrompt = finalPrompt.replace(
-			/\{\{RULES_EDITING_SEARCH_REPLACE_DETAIL\}\}/g,
-			rulesComponents.searchReplaceDetail,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_EDITING_PREFER_OTHER_TOOLS\}\}/g, rulesComponents.preferOtherTools)
-		finalPrompt = finalPrompt.replace(
-			/\{\{RULES_EDITING_WRITE_TO_FILE_DETAIL\}\}/g,
-			rulesComponents.writeToFileDetail,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_MODE_RESTRICTIONS\}\}/g, rulesComponents.modeRestrictions)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_PROJECT_CONTEXT\}\}/g, rulesComponents.projectContext)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_CODE_CHANGE_CONTEXT\}\}/g, rulesComponents.codeChangeContext)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_MINIMIZE_QUESTIONS\}\}/g, rulesComponents.minimizeQuestions)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_ASK_FOLLOWUP_USAGE\}\}/g, rulesComponents.askFollowupUsage)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_EXECUTE_COMMAND_OUTPUT\}\}/g, rulesComponents.executeCommandOutput)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_USER_PROVIDED_CONTENT\}\}/g, rulesComponents.userProvidedContent)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_GOAL_ORIENTED\}\}/g, rulesComponents.goalOriented)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_BROWSER_ACTION_USAGE\}\}/g, rulesComponents.browserActionUsage)
-		finalPrompt = finalPrompt.replace(
-			/\{\{RULES_NO_CONVERSATIONAL_ENDINGS\}\}/g,
-			rulesComponents.noConversationalEndings,
-		)
-		finalPrompt = finalPrompt.replace(
-			/\{\{RULES_NO_CONVERSATIONAL_STARTERS\}\}/g,
-			rulesComponents.noConversationalStarters,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_IMAGE_USAGE\}\}/g, rulesComponents.imageUsage)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_ENV_DETAILS_USAGE\}\}/g, rulesComponents.envDetailsUsage)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_ACTIVE_TERMINALS\}\}/g, rulesComponents.activeTerminals)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_MCP_OPERATIONS\}\}/g, rulesComponents.mcpOperations)
-		finalPrompt = finalPrompt.replace(/\{\{RULES_WAIT_FOR_CONFIRMATION\}\}/g, rulesComponents.waitForConfirmation)
-		finalPrompt = finalPrompt.replace(/\{\{SYSTEM_INFO_SECTION\}\}/g, systemInfoComponents.fullSection)
-		finalPrompt = finalPrompt.replace(/\{\{SYSTEM_INFO_OS\}\}/g, systemInfoComponents.os)
-		finalPrompt = finalPrompt.replace(/\{\{SYSTEM_INFO_SHELL\}\}/g, systemInfoComponents.shell)
-		finalPrompt = finalPrompt.replace(/\{\{SYSTEM_INFO_HOME_DIR\}\}/g, systemInfoComponents.homeDir)
-		finalPrompt = finalPrompt.replace(/\{\{SYSTEM_INFO_CWD\}\}/g, systemInfoComponents.cwd)
-		finalPrompt = finalPrompt.replace(
-			/\{\{SYSTEM_INFO_ENV_DETAILS_EXPLANATION\}\}/g,
-			systemInfoComponents.envDetailsExplanation,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{OBJECTIVE_SECTION\}\}/g, objectiveSection)
-		// Add custom instruction placeholders
-		finalPrompt = finalPrompt.replace(/\{\{CUSTOM_INSTRUCTIONS_SECTION\}\}/g, customInstructions.fullSection)
-		finalPrompt = finalPrompt.replace(
-			/\{\{CUSTOM_INSTRUCTIONS_LANGUAGE_PREFERENCE\}\}/g,
-			customInstructions.languagePreference,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{CUSTOM_INSTRUCTIONS_GLOBAL\}\}/g, customInstructions.globalInstructions)
-		finalPrompt = finalPrompt.replace(
-			/\{\{CUSTOM_INSTRUCTIONS_MODE_SPECIFIC\}\}/g,
-			customInstructions.modeSpecificInstructions,
-		)
-		finalPrompt = finalPrompt.replace(
-			/\{\{CUSTOM_INSTRUCTIONS_RULES_MODE_SPECIFIC\}\}/g,
-			customInstructions.rulesModeSpecific,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{CUSTOM_INSTRUCTIONS_RULES_GENERIC\}\}/g, customInstructions.rulesGeneric)
-		finalPrompt = finalPrompt.replace(
-			/\{\{CUSTOM_INSTRUCTIONS_RULES_ROOIGNORE\}\}/g,
-			customInstructions.rulesRooIgnore,
-		)
-		finalPrompt = finalPrompt.replace(/\{\{CUSTOM_INSTRUCTIONS_RULES_ALL\}\}/g, customInstructions.rulesAll)
-		// TODO: Consider adding other placeholders like {{CWD}}, {{MODE_SLUG}}, {{LANGUAGE}} if useful
+		// Define the mapping of placeholders to their values
+		const placeholderMap: Record<string, string> = {
+			"{{ROLE_DEFINITION}}": roleDefinition,
+			"{{SHARED_TOOL_USE_SECTION}}": sharedToolUseSection,
+			"{{TOOL_DESCRIPTIONS}}": toolDescriptions,
+			"{{TOOL_USE_GUIDELINES}}": toolUseGuidelines,
+			"{{MCP_SERVERS_SECTION}}": mcpServersSection,
+			"{{CAPABILITIES_SECTION}}": capabilitiesSection,
+			"{{MODES_SECTION}}": modesSection,
+			"{{OBJECTIVE_SECTION}}": objectiveSection,
+			// System Info
+			"{{SYSTEM_INFO_SECTION}}": systemInfoComponents.fullSection,
+			"{{SYSTEM_INFO_OS}}": systemInfoComponents.os,
+			"{{SYSTEM_INFO_SHELL}}": systemInfoComponents.shell,
+			"{{SYSTEM_INFO_HOME_DIR}}": systemInfoComponents.homeDir,
+			"{{SYSTEM_INFO_CWD}}": systemInfoComponents.cwd,
+			"{{SYSTEM_INFO_ENV_DETAILS_EXPLANATION}}": systemInfoComponents.envDetailsExplanation,
+			// Rules
+			"{{RULES_SECTION}}": rulesComponents.fullSection,
+			"{{RULES_BASE_DIR}}": rulesComponents.baseDir,
+			"{{RULES_RELATIVE_PATHS}}": rulesComponents.relativePaths,
+			"{{RULES_NO_CD}}": rulesComponents.noCd,
+			"{{RULES_NO_HOME_CHAR}}": rulesComponents.noHomeChar,
+			"{{RULES_EXECUTE_COMMAND_CONTEXT}}": rulesComponents.executeCommandContext,
+			"{{RULES_SEARCH_FILES_USAGE}}": rulesComponents.searchFilesUsage,
+			"{{RULES_NEW_PROJECT_STRUCTURE}}": rulesComponents.newProjectStructure,
+			"{{RULES_EDITING_TOOLS_AVAILABLE}}": rulesComponents.availableTools,
+			"{{RULES_EDITING_INSERT_CONTENT_DETAIL}}": rulesComponents.insertContentDetail,
+			"{{RULES_EDITING_SEARCH_REPLACE_DETAIL}}": rulesComponents.searchReplaceDetail,
+			"{{RULES_EDITING_PREFER_OTHER_TOOLS}}": rulesComponents.preferOtherTools,
+			"{{RULES_EDITING_WRITE_TO_FILE_DETAIL}}": rulesComponents.writeToFileDetail,
+			"{{RULES_MODE_RESTRICTIONS}}": rulesComponents.modeRestrictions,
+			"{{RULES_PROJECT_CONTEXT}}": rulesComponents.projectContext,
+			"{{RULES_CODE_CHANGE_CONTEXT}}": rulesComponents.codeChangeContext,
+			"{{RULES_MINIMIZE_QUESTIONS}}": rulesComponents.minimizeQuestions,
+			"{{RULES_ASK_FOLLOWUP_USAGE}}": rulesComponents.askFollowupUsage,
+			"{{RULES_EXECUTE_COMMAND_OUTPUT}}": rulesComponents.executeCommandOutput,
+			"{{RULES_USER_PROVIDED_CONTENT}}": rulesComponents.userProvidedContent,
+			"{{RULES_GOAL_ORIENTED}}": rulesComponents.goalOriented,
+			"{{RULES_BROWSER_ACTION_USAGE}}": rulesComponents.browserActionUsage,
+			"{{RULES_NO_CONVERSATIONAL_ENDINGS}}": rulesComponents.noConversationalEndings,
+			"{{RULES_NO_CONVERSATIONAL_STARTERS}}": rulesComponents.noConversationalStarters,
+			"{{RULES_IMAGE_USAGE}}": rulesComponents.imageUsage,
+			"{{RULES_ENV_DETAILS_USAGE}}": rulesComponents.envDetailsUsage,
+			"{{RULES_ACTIVE_TERMINALS}}": rulesComponents.activeTerminals,
+			"{{RULES_MCP_OPERATIONS}}": rulesComponents.mcpOperations,
+			"{{RULES_WAIT_FOR_CONFIRMATION}}": rulesComponents.waitForConfirmation,
+			// Custom Instructions
+			"{{CUSTOM_INSTRUCTIONS_SECTION}}": customInstructions.fullSection,
+			"{{CUSTOM_INSTRUCTIONS_LANGUAGE_PREFERENCE}}": customInstructions.languagePreference,
+			"{{CUSTOM_INSTRUCTIONS_GLOBAL}}": customInstructions.globalInstructions,
+			"{{CUSTOM_INSTRUCTIONS_MODE_SPECIFIC}}": customInstructions.modeSpecificInstructions,
+			"{{CUSTOM_INSTRUCTIONS_RULES_MODE_SPECIFIC}}": customInstructions.rulesModeSpecific,
+			"{{CUSTOM_INSTRUCTIONS_RULES_GENERIC}}": customInstructions.rulesGeneric,
+			"{{CUSTOM_INSTRUCTIONS_RULES_ROOIGNORE}}": customInstructions.rulesRooIgnore,
+			"{{CUSTOM_INSTRUCTIONS_RULES_ALL}}": customInstructions.rulesAll,
+			// Add requested placeholders
+			"{{CWD}}": cwd,
+			"{{MODE_SLUG}}": currentMode.slug,
+			"{{LANGUAGE}}": language ?? formatLanguage(vscode.env.language),
+		}
+
+		// Perform substitutions using reduce for better maintainability
+		const finalPrompt = Object.entries(placeholderMap).reduce((prompt, [placeholder, value]) => {
+			// Escape placeholder for regex usage
+			const escapedPlaceholder = placeholder.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")
+			return prompt.replace(new RegExp(escapedPlaceholder, "g"), value || "") // Replace with empty string if value is undefined/null
+		}, fileCustomSystemPrompt)
 
 		return finalPrompt
 	}
